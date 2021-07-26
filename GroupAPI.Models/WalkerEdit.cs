dc85d635696e9bc6e651f008a69957479838aa69
Jim Stewart <jamstew62@gmail.com>
2021-07-26 16:48:52 -0400

    initial walker models

---
+++ GroupAPI.Models/WalkerEdit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupAPI.Models
+{
+    class WalkerEdit
+    {
+        public int WalkerId { get; set; }
+        public string WalkerName { get; set; }
+    }
+}
